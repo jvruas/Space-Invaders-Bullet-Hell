2e11cbe7fc7d756a2da39aec114adbd8fb02f701
JvRuas <contato.jvruas@gmail.com>
2021-05-13 12:39:57 -0300

    Primeiro Script do Data Base

--- models/data/db_remake_space_invaders.sql
+++ models/data/db_remake_space_invaders.sql
@@ -1,1 +1,17 @@
-CREATE DATABASE db_remake_space_invaders;
+-- CREATE DATABASE db_remake_space_invaders;
+USE db_remake_space_invaders;
+
+CREATE TABLE tb_user(
+	idUser INT PRIMARY KEY AUTO_INCREMENT,
+    userName VARCHAR(30) NOT NULL,
+    password VARCHAR(30) NOT NULL,
+    email VARCHAR(80) NOT NULL
+);
+
+CREATE TABLE tb_game_score (
+	idScore INT PRIMARY KEY AUTO_INCREMENT,
+    fkUser INT,
+	FOREIGN KEY (fkUser) REFERENCES tb_user(idUser),
+	PRIMARY KEY (idScore, fkUser),
+    matchScore DECIMAL (8,2)
+);
